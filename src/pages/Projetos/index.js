6a9707c7a3376a47e810f3a87b609139fd144d8e
Vanessa Teixeira <vanessateixeira516@gmail.com>
2021-02-16 23:24:13 -0300

    Tela Projeto mobile

--- src/pages/Projetos/index.js
+++ src/pages/Projetos/index.js
@@ -15,6 +15,7 @@ import {StyleSheet,
 
 import FA from 'react-native-vector-icons/FontAwesome';
 import IO from 'react-native-vector-icons/Ionicons';
+import { poly } from 'react-native/Libraries/Animated/src/Easing';
 
 
 const menu = <IO name="menu" size={30} color="#fff" />;
@@ -40,17 +41,72 @@ const Projetos = ({navigation}) =>{
                     {/* <Text style={styles.h1}>Projetos</Text> */}
 
                     <View style={styles.block1}>
-                        .
+                   
+                    <View style= {styles.img1}>
+                    <Image source={require('../../../assets/2.png')}
+                    style={styles.p1}>
+                    </Image>
                     </View>
-                    <View style={styles.block2}>
-                        .
-                    </View>
-                    <View style={styles.block3}>
-                        .
-                    </View>
+                    <View style={styles.txt}>
+                    <Text style={styles.h1}>
+                            <center><h3>Nossa Objetivo</h3></center>
+                        </Text >
+                        <Text style={styles.txt2}>
+                        Reverter materias reciclados 
+                        em benefícios para a comunidade.
+                    
 
-
+                        </Text>
+                    </View>
+                    
             </View>
+                    <View style={styles.block2}>
+                    <View style= {styles.img1}>
+                    <Image source={require('../../../assets/1.png')}
+                    style={styles.p1}>
+                    </Image>
+                    </View>
+                    <View style={styles.txt}>
+                        <Text style={styles.h1}>
+                        <center><h3>Resultado</h3></center>
+                        </Text>
+                        
+                            <Text style={styles.txt2}>
+                                Com o recurso convertidos pela ação de reciclar, todos serão befeficiados.
+
+
+                        </Text>
+                    </View>
+
+             </View>
+                    <View style={styles.block3}>
+                    <View style= {styles.img1}>
+                    <Image source={require('../../../assets/3.png')}
+                    style={styles.p1}>
+                    </Image>
+                    </View>
+                    <View style={styles.txt}>
+                    <Text style={styles.h1}>
+                        <center><h3>Assim</h3></center>
+                        </Text>
+
+                        <Text style={styles.txt2}>
+                        Conscietizando a impôrtancia de
+                        adotar estilos sustentáveis de vida.
+                        </Text>
+                    </View>
+
+             </View>
+                    
+                    
+                    
+                    
+                    
+                    
+            </View>
+
+
+            
 
             {/* <View style={styles.footer}>
 
@@ -94,21 +150,98 @@ const styles = StyleSheet.create({
         alignItems: 'center',
         width:'100%',
         height:'30%',
-        backgroundColor:'#c3c3c3'
+        backgroundColor:'#F8F9FA',
+        flexDirection:'row',
+        justifyContent:'space-around'
+
     },
     block2:{
         justifyContent: 'center',
         alignItems: 'center',
         width:'100%',
         height:'30%',
-        backgroundColor:'#c3c3c3'
+        backgroundColor:'#F8F9FA',
+        flexDirection:'row',
+        justifyContent:'space-around'
     },
     block3:{
         justifyContent: 'center',
         alignItems: 'center',
         width:'100%',
         height:'30%',
-        backgroundColor:'#c3c3c3'
-    }
+        backgroundColor:'#F8F9FA',
+        flexDirection:'row',
+        justifyContent:'space-around'
+        
 
+    },
+
+    
+    p1: {
+        width: 140,
+        height: 140,
+        borderRadius: 100,
+        position:'absolute',
+        marginLeft:5,
+        left:0
+    },
+
+    p2:{
+        fontSize: 20,
+        textAlign:"left",
+        },
+
+
+    img1:{
+        width:180,
+        height:170,
+        //backgroundColor:'red'
+       
+
+    },
+
+    txt: {
+        width:180,
+        height:160,
+       // backgroundColor:'red'
+    },
+
+    txt2: {
+        fontSize: 16,
+        justifyContent: 'center',
+        //alignItems: 'center',
+        color: '#003',
+        height: 118,
+        marginLeft: 3,
+        marginRight:3,
+        display: 'flex',
+        textAlign:"center"
+        
+    },
+
+    h1:{
+        textAlign:"auto",
+        fontFamily:"arial",
+        fontStyle:"italic",
+        marginTop:3
+    
+
+        
+        
+    
+    
+   
+         },
+    
 })
+    
+   
+    
+
+        
+        
+        
+
+
+
+
